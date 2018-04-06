503b3cc76f55267fe8da870c3d87e7d6f4fd9027
petergavagan <35042117+petergavagan@users.noreply.github.com>
2018-04-05 20:07:26 -0400

    Update decade_down.py

--- decade_down.py
+++ decade_down.py
@@ -3,8 +3,8 @@ print("Richie!! Decade is down!!")
 while True:
     user_input = input("Who should you call?")
 	if user_input == "htc":
-	    print("You fixed Decade!")
-		break
+	print("You fixed Decade!")
+	break
 	else:
 	    print("Keep trying!")
 		
