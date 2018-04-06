145d31cc765ed5b7c65f2f97e73bfb53a5ba1dd0
petergavagan <35042117+petergavagan@users.noreply.github.com>
2018-04-05 20:05:47 -0400

    Add files via upload

---
+++ decade_down.py
@@ -0,0 +1,10 @@
+print("Richie!! Decade is down!!")
+
+while True:
+    user_input = input("Who should you call?")
+	if user_input == "htc":
+	    print("You fixed Decade!")
+		break
+	else:
+	    print("Keep trying!")
+		
